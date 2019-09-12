35e467bcdda73d1cca877524b6f0c471b2b845f9
Jacqueline Remmel and Lauren Musante <lauren@>
2019-09-12 14:38:21 -0700

    Add answer options, correct answer, and API call to giphy.

--- src/main.js
+++ src/main.js
@@ -2,23 +2,49 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import { getTrivia } from "./scripts.js";
+import { getTrivia, getGif, generateOptions } from "./scripts.js";
 
 $(document).ready(function(){
   $("#trivia").submit(function(event){
     event.preventDefault();
+    $(".answer-output").hide();
+    $(".gif-output").hide();
+    $("#show-options").hide();
+    $(".answer-options").hide();
     let category = $("#categories").val();
     let difficulty = $("#difficulty").val();
-    let triviaQuestion = getTrivia(category, difficulty); // will return new instance of promise object
-    triviaQuestion.then(function(response){
-      const question = JSON.parse(response); //main nameless array in Postman
-      console.log(question);
-      if (question.results[0].type === "boolean") {
-        $("#trivia-question").html(`<p>True or False: ${question.results[0].question}</p>`);
-      } else {
-        $("#trivia-question").html(`<p>${question.results[0].question}</p>`);
-      }
-      $(".trivia-output").show();
-    });
+
+    getTrivia(category, difficulty)
+      .then(function(response) {
+        const question = JSON.parse(response);
+        if (question.results[0].type === "boolean") {
+          $("#trivia-question").html(`<p>True or False: ${question.results[0].question}</p>`);
+        } else {
+          $("#trivia-question").html(`<p>${question.results[0].question}</p>`);
+        }
+        $(".trivia-output").show();
+        if (question.results[0].type === "multiple") {
+          let answerOptions = generateOptions(question.results[0].incorrect_answers, question.results[0].correct_answer);
+          let answerList = answerOptions.join(" <br>");
+          $(".answer-options").html(`${answerList}`);
+          $("#show-options").show();
+        }
+        let answer = question.results[0].correct_answer;
+        $(".answer-output").html(`<p><strong>Correct Answer: ${answer}</strong></p>`);
+        let answerArray = answer.split(" ");
+        return getGif(answerArray[0]);
+      })
+      .then(function(response) {
+        let gifResponse = JSON.parse(response);
+        let gif = gifResponse.data.images.downsized_medium.url;
+        $(".gif-output").html(`<img src="${gif}">`);
+        $("#show-options").click(function() {
+          $(".answer-options").show();
+        });
+        $("#show-answer").click(function(){
+        $(".answer-output").show();
+        $(".gif-output").show();
+        });
+      });
   });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -22,3 +22,28 @@ export function getTrivia(category, difficulty) {
     request.send();
   });
 }
+
+export function getGif(topic) {
+  return new Promise(function(resolve, reject) {
+    let url = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&rating=g&tag=${topic}`;
+    let request = new XMLHttpRequest();
+    request.onload = function(){
+      if(this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error(request.statusText));
+      }
+    }
+    request.open("GET", url, true);
+    request.send();
+  });
+}
+
+export function generateOptions(answerOptions, correctAnswer) {
+  console.log(answerOptions);
+  const number = Math.round(Math.random() * (answerOptions.length + 1));
+  console.log(number);
+  answerOptions.splice(number, 0, correctAnswer);
+  console.log(answerOptions);
+  return answerOptions;
+}
