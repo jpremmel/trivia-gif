c46ff7c0794902d5feb6a833e2afdd0965367b93
Jacqueline Remmel and Lauren Musante <lauren@>
2019-09-12 08:42:40 -0700

    Add project files

---
+++ README.md
@@ -0,0 +1,30 @@
+# _Dino Generator_
+
+#### _A page that generates a random dinosaur name, gif, and fact, 12 Sept 2019_
+
+#### By _**Lauren Musante and Jacqueline Remmel**_
+
+## Description
+
+_The primary purpose of this project was to practice making API calls._
+
+## Setup/Installation Requirements
+
+* _Clone this repository_
+* _Navigate to the project directory and run the command "npm install"_
+* _Go to https://developers.giphy.com/dashboard/ and log in to your account (or create an account and log in)_
+* _Create an App on your Giphy Dashboard to obtain an API key_
+* _In the project directory, create a file called ".env"_
+* _Open the .env file and store your API key using the format "API_KEY = your-API-key-here"_
+* _Save the .env file_
+* _Run the command "npm run start"
+
+## Technologies Used
+
+_JavaScript, jQuery, Webpack, CSS, Bootstrap, HTML_
+
+### License
+
+*Open-source*
+
+Copyright (c) 2019 **_Lauren Musante and Jacqueline Remmel_**
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+entry: './src/main.js',
+output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'dist')
+},
+devtool: 'eval-source-map',
+devServer: {
+  contentBase: './dist'
+},
+plugins: [
+  new Dotenv(),
+  new UglifyJsPlugin({ sourceMap: true }),
+  new CleanWebpackPlugin(['dist']),
+  new CopyWebpackPlugin([
+    {from:'src/img',to:'img'}
+  ]),
+  new HtmlWebpackPlugin({
+    title: 'My Project',
+    template: './src/index.html',
+    inject: 'body'
+  })
+],
+module: {
+  rules: [
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    },
+    {
+      test: /\.js$/,
+      exclude: [
+        /node_modules/,
+        /spec/
+      ],
+      loader: "eslint-loader"
+    },
+    {
+
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
+        'file-loader',
+        {
+        loader: 'image-webpack-loader',
+        options: {
+          bypassOnDebug: true, // webpack@1.x
+          disable: true, // webpack@2.x and newer
+        }
+      }]
+
+    },
+    {
+      test: /\.js$/,
+      exclude: [
+        /node_modules/,
+        /spec/
+      ],
+      loader: "babel-loader",
+      options: {
+        presets: ['es2015']
+      }
+    }
+  ]
+}
+};
